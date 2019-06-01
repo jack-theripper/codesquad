a89d796edcdabf31cc1c5d117e22e5166d728617
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2019-06-01 15:36:03 +0300

    Автозагрузка composer

--- app/config/loader.php
+++ app/config/loader.php
@@ -8,16 +8,23 @@ $loader = new Loader();
  * Register Namespaces
  */
 $loader->registerNamespaces([
-    'Codesquad\Models' => APP_PATH . '/common/models/',
-    'Codesquad'        => APP_PATH . '/common/library/',
+	'Codesquad\Models' => APP_PATH.'/common/models/',
+	'Codesquad'        => APP_PATH.'/common/library/'
 ]);
 
 /**
  * Register module classes
  */
 $loader->registerClasses([
-    'Codesquad\Modules\Frontend\Module' => APP_PATH . '/modules/frontend/Module.php',
-    'Codesquad\Modules\Cli\Module'      => APP_PATH . '/modules/cli/Module.php'
+	'Codesquad\Modules\Frontend\Module' => APP_PATH.'/modules/frontend/Module.php',
+	'Codesquad\Modules\Cli\Module'      => APP_PATH.'/modules/cli/Module.php'
+]);
+
+/**
+ * Register composer autoload
+ */
+$loader->registerFiles([
+	BASE_PATH.'/vendor/autoload.php'
 ]);
 
 $loader->register();
