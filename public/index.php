b5ef4605849619b179872a133e873b6a906a0ce2
Dmitry Arhitector <dmitry.arhitector@yandex.ru>
2019-06-01 16:57:19 +0300

    Рефакторинг точки входа в приложении web

--- public/index.php
+++ public/index.php
@@ -1,3 +1,61 @@
 <?php
 
-require '../app/bootstrap_web.php';
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Application;
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH.'/app');
+
+try
+{
+	/**
+	 * The FactoryDefault Dependency Injector automatically registers the services that
+	 * provide a full stack framework. These default services can be overidden with custom ones.
+	 */
+	$di = new FactoryDefault();
+	
+	/**
+	 * Include general services
+	 */
+	require APP_PATH.'/config/services.php';
+	
+	/**
+	 * Include web environment specific services
+	 */
+	require APP_PATH.'/config/services_web.php';
+	
+	/**
+	 * Get config service for use in inline setup below
+	 */
+	$config = $di->getConfig();
+	
+	/**
+	 * Handle the request
+	 */
+	$application = new Application($di);
+	
+	/**
+	 * Register application modules
+	 */
+	$application->registerModules([
+		'frontend' => [
+			'className' => Codesquad\Modules\Frontend\Module::class
+		]
+	]);
+	
+	/**
+	 * Include Autoloader
+	 */
+	include APP_PATH.'/config/loader.php';
+	
+	/**
+	 * Include routes
+	 */
+	require APP_PATH.'/config/routes.php';
+	
+	$application->handle()->send();
+}
+catch (Exception $exception)
+{
+	throw $exception;
+}
